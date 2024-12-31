395fc028640ac3f4bb9be030847a66a2654361b1
unknown <sashenhasindu@gmail.com>
2024-12-31 15:30:23 +1100

    🐛 Bug Fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "scontent.cdninstagram.com",
-      },
-    ],
-  },
+  images: ["scontent-iad3-1.cdninstagram.com"],
 };
 
 export default nextConfig;
