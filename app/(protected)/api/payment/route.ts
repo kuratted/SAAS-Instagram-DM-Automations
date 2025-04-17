b7a8820758522d5c4dd6b76b846217ecb8eeac67
kuratted <101197329+kuratted@users.noreply.github.com>
2025-04-17 21:50:05 +0530

    Update route.ts

--- app/(protected)/api/payment/route.ts
+++ app/(protected)/api/payment/route.ts
@@ -6,23 +6,23 @@ export async function GET() {
   const user = await currentUser();
   if (!user) return NextResponse.json({ status: 404 });
 
-  // const priceId = process.env.STRIPE_SUBSCRIPTION_PRICE_ID;
+  // // const priceId = process.env.STRIPE_SUBSCRIPTION_PRICE_ID;
 
-  string session = ""
+  // string session = ""
 
-  // await stripe.checkout.sessions.create({
-  //   mode: "subscription",
-  //   line_items: [{ price: priceId, quantity: 1 }],
-  //   success_url: `${process.env.NEXT_PUBLIC_HOST_URL}/payment?session_id={CHECKOUT_SESSION_ID}`,
-  //   cancel_url: `${process.env.NEXT_PUBLIC_HOST_URL}/payment?cancel=true`,
-  // });
+  // // await stripe.checkout.sessions.create({
+  // //   mode: "subscription",
+  // //   line_items: [{ price: priceId, quantity: 1 }],
+  // //   success_url: `${process.env.NEXT_PUBLIC_HOST_URL}/payment?session_id={CHECKOUT_SESSION_ID}`,
+  // //   cancel_url: `${process.env.NEXT_PUBLIC_HOST_URL}/payment?cancel=true`,
+  // // });
 
-  if (session) {
-    return NextResponse.json({
-      status: 200,
-      session_url: session.url,
-    });
-  }
+  // if (session) {
+  //   return NextResponse.json({
+  //     status: 200,
+  //     session_url: session.url,
+  //   });
+  // }
 
   return NextResponse.json({ status: 400 });
 }
